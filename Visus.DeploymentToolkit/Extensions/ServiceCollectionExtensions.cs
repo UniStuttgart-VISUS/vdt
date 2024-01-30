b5536b24b4b8ce36e9ec888d61ec14a3354329a7
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2024-01-30 15:43:22 +0100

    Fixed file header.

--- Visus.DeploymentToolkit/Extensions/ServiceCollectionExtensions.cs
+++ Visus.DeploymentToolkit/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// <copyright file="ServiceCollectionExtensions.cs" company="Visualisierungsinstitut der Universität Stuttgart">
 // Copyright © 2024 Visualisierungsinstitut der Universität Stuttgart.
-// Licensed under the MIT licence. See LICENCE file for details
+// Licensed under the MIT licence. See LICENCE file for details.
 // </copyright>
 // <author>Christoph Müller</author>
 
