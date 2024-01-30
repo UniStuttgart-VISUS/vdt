eddd8cda67d35f62b478edf4eef2120b08073223
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2024-01-30 15:41:16 +0100

    Added disk management.

---
+++ Visus.DeploymentToolkit/IDiskManagement.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IDiskManagement.cs" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2024 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details
+// </copyright>
+// <author>Christoph Müller</author>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Visus.DeploymentToolkit {
+
+    /// <summary>
+    /// Defines the interface of the disk management service.
+    /// </summary>
+    public interface IDiskManagement {
+    }
+}
