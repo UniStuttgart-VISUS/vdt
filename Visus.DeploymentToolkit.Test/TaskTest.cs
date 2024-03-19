0d40f339c4129453c03fe4abb461259d9ca4fdd6
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2024-03-19 11:38:50 +0100

    Added test for RunCommand.

---
+++ Visus.DeploymentToolkit.Test/TaskTest.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="TaskTest.cs" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2024 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+using Microsoft.Extensions.Logging;
+using Moq;
+using Visus.DeploymentToolkit.Contracts;
+using Visus.DeploymentToolkit.Tasks;
+
+
+namespace Visus.DeploymentToolkit.Test {
+
+    /// <summary>
+    /// Tests for <see cref="ITask"/>s.
+    /// </summary>
+    [TestClass]
+    public sealed class TaskTest {
+
+        [TestMethod]
+        public async Task TestRunCommand() {
+            var task = new RunCommand(Mock.Of<ILogger<RunCommand>>());
+            task.Path = @"c:\Windows\System32\cmd.exe";
+            task.Arguments = "/c @(call)";
+            task.SucccessExitCodes = new[] { 1 };
+            await task.ExecuteAsync(Mock.Of<IState>());
+        }
+    }
+}
