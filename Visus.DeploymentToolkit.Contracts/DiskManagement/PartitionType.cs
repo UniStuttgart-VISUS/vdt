278360802c4c0675c0583c29a1583d2372a93328
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2024-04-05 11:23:28 +0200

    Rework PartitionType such that it can be created from input of VDS.

--- Visus.DeploymentToolkit.Contracts/DiskManagement/PartitionType.cs
+++ Visus.DeploymentToolkit.Contracts/DiskManagement/PartitionType.cs
@@ -15,22 +15,23 @@ namespace Visus.DeploymentToolkit.DiskManagement {
     /// <summary>
     /// Represents a type of partition.
     /// </summary>
-    public sealed class PartitionType {
+    public sealed class PartitionType : IEquatable<PartitionType> {
 
         #region Public class properties
         /// <summary>
-        /// Gets all well-known partition types.
+        /// Gets all well-known partition types defined as fields of this class.
         /// </summary>
         public static IEnumerable<PartitionType> All => _all.Value;
         #endregion
 
+        #region Public class methods
         /// <summary>
         /// Gets all partition types with matching GPT GUID.
         /// </summary>
         /// <param name="guid"></param>
         /// <returns></returns>
         public static IEnumerable<PartitionType> FromGpt(Guid guid)
-            => All.Where(t => t.Guid == guid);
+            => All.Where(t => t.Gpt == guid);
 
         /// <summary>
         /// Gets all partition types with matching MBR ID.
@@ -38,77 +39,200 @@ namespace Visus.DeploymentToolkit.DiskManagement {
         /// <param name="id"></param>
         /// <returns></returns>
         public static IEnumerable<PartitionType> FromMbr(byte id)
-            => All.Where(t => t.ID == RemoveMsftHidden(id));
+            => All.Where(t => t.Mbr == RemoveMsftHidden(id));
+        #endregion
 
         #region Known partition types
-        public static readonly PartitionType Fat12 = new(
-            "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", 0x01, "FAT12");
+        /// <summary>
+        /// FAT 12.
+        /// </summary>
+        public static readonly PartitionType Fat12 = new(0x01, "FAT12");
 
-        public static readonly PartitionType Fat16LessThan32Mb = new(
-            "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", 0x04, "FAT16 (< 32MB)");
+        /// <summary>
+        /// FAT16 < 32 MB.
+        /// </summary>
+        public static readonly PartitionType Fat16LessThan32Mb
+            = new(0x04, "FAT16");
 
-        public static readonly PartitionType Fat16 = new(
-            "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", 0x06, "FAT16");
+        /// <summary>
+        /// FAT16.
+        /// </summary>
+        public static readonly PartitionType Fat16 = new(0x06, "FAT16");
 
-        public static readonly PartitionType Ntfs = new(
-            "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", 0x07, "NTFS");
+        /// <summary>
+        /// NTFS or HPFS.
+        /// </summary>
+        public static readonly PartitionType Ntfs = new(0x07, "NTFS");
 
-        public static readonly PartitionType Fat32 = new(
-            "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", 0x0b, "FAT32");
+        /// <summary>
+        /// FAT32.
+        /// </summary>
+        public static readonly PartitionType Fat32 = new(0x0b, "FAT32");
 
-        public static readonly PartitionType Fat32Lba = new(
-            "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", 0x0c, "FAT32 LBA");
+        /// <summary>
+        /// FAT32 LBA.
+        /// </summary>
+        public static readonly PartitionType Fat32Lba = new(0x0c, "FAT32 LBA");
 
-        public static readonly PartitionType Fat16Lba = new(
-            "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", 0x0e, "FAT16 LBA");
+        /// <summary>
+        /// FAT16 LBA.
+        /// </summary>
+        public static readonly PartitionType Fat16Lba = new(0x0e, "FAT16 LBA");
 
+        /// <summary>
+        /// Microsoft basic data (FAT and NTFS on GPT).
+        /// </summary>
+        public static readonly PartitionType MicrosoftBasicData = new(
+            "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data");
+
+        /// <summary>
+        /// Microsoft reserved parittion.
+        /// </summary>
         public static readonly PartitionType WindowsEfi = new(
             "E3C9E316-0B5C-4DB8-817D-F92DF00215AE", 0x0c, "Microsoft reserved");
 
+        /// <summary>
+        /// Windows Recovery Environment.
+        /// </summary>
         public static readonly PartitionType WindowsRe = new(
             "DE94BBA4-06D1-4D40-A16A-BFD50179D6AC", 0x27, "Windows RE");
 
+        /// <summary>
+        /// Data partition of the Windows Logical Disk Manager.
+        /// </summary>
         public static readonly PartitionType WindowsLdmData = new(
-            "AF9B60A0-1431-4F62-BC68-3311714A69AD", 0x42, "Windows LDM data");
+            "AF9B60A0-1431-4F62-BC68-3311714A69AD", "Windows LDM data");
 
-        public static readonly PartitionType WindowsLdmMetadata = new(
-            "5808C8AA-7E8F-42E0-85D2-E1E90434CFB3", 0x42, "Windows LDM metadata");
+        /// <summary>
+        /// Meta-data partition of the Windows Logical Disk Manager.
+        /// </summary>
+        public static readonly PartitionType WindowsLdmMetaData = new(
+            "5808C8AA-7E8F-42E0-85D2-E1E90434CFB3", "Windows LDM meta data");
 
+        /// <summary>
+        /// Windows Storage Spaces (Windows 8 and later).
+        /// </summary>
         public static readonly PartitionType WindowsStorageSpaces = new(
-            "E75CAF8F-F680-4CEE-AFA3-B001E56EFC2D", 0x42, "Windows Storage Spaces");
+            "E75CAF8F-F680-4CEE-AFA3-B001E56EFC2D", "Windows Storage Spaces");
 
+        /// <summary>
+        /// Windows software RAID on MBR disks.
+        /// </summary>
+        public static readonly PartitionType Windows= new(0x42, "Windows");
+
+        /// <summary>
+        /// Linux swap partition.
+        /// </summary>
         public static readonly PartitionType LinuxSwap = new(
             "0657FD6D-A4AB-43C4-84E5-0933C84B4F4F", 0x82, "Linux swap");
 
+        /// <summary>
+        /// Linux native partition.
+        /// </summary>
         public static readonly PartitionType LinuxFileSystem = new(
-            "0FC63DAF-8483-4772-8E79-3D69D8477DE4", 0x82, "Linux file system");
+            "0FC63DAF-8483-4772-8E79-3D69D8477DE4", "Linux file system");
 
+        /// <summary>
+        /// Linux hybrid MBR.
+        /// </summary>
         public static readonly PartitionType LinuxReserved = new(
-            "8DA63339-0007-60C0-C436-083AC8230908", 0x83, "Linux reserved");
+            "8DA63339-0007-60C0-C436-083AC8230908", "Linux reserved");
 
+        /// <summary>
+        /// Linux MBR.
+        /// </summary>
+        public static readonly PartitionType Linux = new(0x83, "Linux");
+
+        /// <summary>
+        /// Linux Logical Volume Manager.
+        /// </summary>
         public static readonly PartitionType Lvm = new(
             "E6D6D379-F507-44C2-A23C-238F2A3DF928", 0x8e, "Linux LVM");
 
+        /// <summary>
+        /// The EFI system partition (EPS) formatted as FAT containing the EFI
+        /// boot firmware.
+        /// </summary>
         public static readonly PartitionType EfiSystem = new(
-            "C12A7328-F81F-11D2-BA4B-00A0C93EC93B", 0xef, "EFI system partition");
+            "C12A7328-F81F-11D2-BA4B-00A0C93EC93B", "EFI system partition");
 
+        /// <summary>
+        /// A partition holding a MBR partition table for virtualisation
+        /// purposes.
+        /// </summary>
         public static readonly PartitionType EfiMbr = new(
-            "024DEE41-33E7-11D3-9D69-0008C781F39F", 0xef, "MBR partition scheme");
+            "024DEE41-33E7-11D3-9D69-0008C781F39F", "MBR partition scheme");
 
+        /// <summary>
+        /// The EFI boot partition used by GRUB to start on BIOS-based PCs.
+        /// </summary>
         public static readonly PartitionType EfiBoot = new(
-            "21686148-6449-6E6F-744E-656564454649", 0xef, "BIOS boot partition");
+            "21686148-6449-6E6F-744E-656564454649", "BIOS boot partition");
+
+        /// <summary>
+        /// An EFI parition on MBR disks.
+        /// </summary>
+        public static readonly PartitionType Efi = new(0xef, "EFI");
+        #endregion
+
+        #region Public constructors
+        /// <summary>
+        /// Initialises a new instance.
+        /// </summary>
+        /// <param name="guid">The GUID of the GPT partition type.</param>
+        /// <param name="name">The human readable name of the partition type. If
+        /// this is <c>null</c>, the constructor will look up the name from the
+        /// well-known partition types or use the GUID as final fallback.</param>
+        public PartitionType(Guid guid, string? name) {
+            this.Gpt = guid;
+            this.Name = name
+                ?? FromGpt(guid).FirstOrDefault()?.Name
+                ?? guid.ToString("B");
+        }
+
+        /// <summary>
+        /// Initialises a new instance.
+        /// </summary>
+        /// <param name="id">The MBR partition ID.</param>
+        /// <param name="name">The human readable name of the partition type. If
+        /// this is <c>null</c>, the constructor will look up the name from the
+        /// well-known partition types or use the partition ID as final
+        /// fallback.</param>
+        public PartitionType(byte id, string? name) {
+            this.Mbr = id;
+            this.Name = name
+                ?? FromMbr(id).FirstOrDefault()?.Name
+                ?? id.ToString("X2");
+        }
         #endregion
 
         #region Public properties
         /// <summary>
         /// Gets the GPT GUID of the partition type.
         /// </summary>
-        public Guid Guid { get; }
+        /// <remarks>
+        /// This property might be <c>null</c> for MBR-only partition types.
+        /// </remarks>
+        public Guid? Gpt { get; }
+
+        /// <summary>
+        /// Answer whether the partition type is for GUID partition tables.
+        /// </summary>
+        public bool IsGpt => this.Gpt != null;
+
+        /// <summary>
+        /// Answer whether the partition type is for Master Boot Record
+        /// partition tables.
+        /// </summary>
+        public bool IsMbr => this.Mbr != null;
 
         /// <summary>
         /// Gets the MBR ID of the partition type.
         /// </summary>
-        public byte ID { get; }
+        /// <remarks>
+        /// This property might be <c>null</c> for GPT-only partition types.
+        /// </remarks>
+        public byte? Mbr { get; }
 
         /// <summary>
         /// Gets the human readable name of the partition type.
@@ -117,6 +241,30 @@ namespace Visus.DeploymentToolkit.DiskManagement {
         #endregion
 
         #region Public methods
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
+            => this.Equals(obj as PartitionType);
+
+        /// <inheritdoc />
+        public bool Equals(PartitionType? other) {
+            if (other == null) {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, other)) {
+                return true;
+            }
+
+            return ((this.Gpt == other.Gpt) && (this.Mbr == other.Mbr));
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode() {
+            return typeof(PartitionType).GetHashCode()
+                ^ this.Gpt?.GetHashCode() ?? 0
+                ^ this.Mbr?.GetHashCode() ?? 0;
+        }
+
         /// <inheritdoc />
         public override string ToString() => this.Name;
         #endregion
@@ -142,9 +290,33 @@ namespace Visus.DeploymentToolkit.DiskManagement {
         #endregion
 
         #region Private constructors
-        private PartitionType(string guid, byte mbr, string name) {
-            this.Guid = Guid.Parse(guid);
-            this.ID = mbr;
+        /// <summary>
+        /// Initialises a new instance.
+        /// </summary>
+        /// <param name="guid">The string representation of the GPT partition
+        /// type.</param>
+        /// <param name="name">The human-readable name of the partition type.
+        /// </param>
+        /// <exception cref="ArgumentNullException">If <paramref name="name"/>
+        /// is <c>null</c>.</exception>
+        private PartitionType(string guid, string name) {
+            this.Gpt = Guid.Parse(guid);
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        /// <summary>
+        /// Initialises a new instance.
+        /// </summary>
+        /// <param name="guid">The string representation of the GPT partition
+        /// type.</param>
+        /// <param name="id">The MBR ID.</param>
+        /// <param name="name">The human-readable name of the partition type.
+        /// </param>
+        /// <exception cref="ArgumentNullException">If <paramref name="name"/>
+        /// is <c>null</c>.</exception>
+        private PartitionType(string guid, byte id, string name) {
+            this.Gpt = Guid.Parse(guid);
+            this.Mbr = id;
             this.Name = name ?? throw new ArgumentNullException(nameof(name));
         }
         #endregion
