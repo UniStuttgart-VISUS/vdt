fc17cff2a11833986727e714be4bbc053a3ae349
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2025-12-01 15:08:48 +0100

    Added the ability to publish bootstrapper and agent binaries as well as unattend templates.

--- Visus.DeploymentToolkit/Tasks/PrepareDeploymentShare.cs
+++ Visus.DeploymentToolkit/Tasks/PrepareDeploymentShare.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Visus.DeploymentToolkit.DeploymentShare;
@@ -16,6 +15,7 @@ using Visus.DeploymentToolkit.Extensions;
 using Visus.DeploymentToolkit.Properties;
 using Visus.DeploymentToolkit.Services;
 using Visus.DeploymentToolkit.Validation;
+using SysPath = System.IO.Path;
 
 
 namespace Visus.DeploymentToolkit.Tasks {
@@ -28,8 +28,11 @@ namespace Visus.DeploymentToolkit.Tasks {
         #region Public constructors
         public PrepareDeploymentShare(IState state,
                 IDirectory directory,
+                ICopy copy,
                 ILogger<PrepareDeploymentShare> logger)
                 : base(state, logger) {
+            this._copy = copy
+                ?? throw new ArgumentNullException(nameof(copy));
             this._directory = directory
                 ?? throw new ArgumentNullException(nameof(directory));
             this.Name = Resources.PrepareDeploymentShare;
@@ -38,11 +41,35 @@ namespace Visus.DeploymentToolkit.Tasks {
 
         #region Public properties
         /// <summary>
+        /// Gets or sets the location of the agent binaries to be deployed to
+        /// the share.
+        /// </summary>
+        /// <remarks>
+        /// The path to the directory is expected.
+        /// </remarks>
+        public string? Agent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location of the bootstrapper binaries to be
+        /// deployed to the share.
+        /// </summary>
+        /// <remarks>
+        /// The path to the directory is expected.
+        /// </remarks>
+        public string? Bootstrapper { get; set; }
+
+        /// <summary>
         /// Gets or sets the location of the deployment share.
         /// </summary>
         [Required]
         [EmptyDirectory]
         public string Path { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the location of the template files.
+        /// </summary>
+        public string? Templates { get; set; }
+
         #endregion
 
         #region Public methods
@@ -65,7 +92,7 @@ namespace Visus.DeploymentToolkit.Tasks {
             cancellationToken.ThrowIfCancellationRequested();
 
             {
-                var p = System.IO.Path.Combine(this.Path, Layout.BinaryPath);
+                var p = SysPath.Combine(this.Path, Layout.BinaryPath);
                 this._logger.LogInformation("Creating directory {Path} for "
                     + "binaries.", p);
                 await this._directory.CreateAsync(p).ConfigureAwait(false);
@@ -73,7 +100,7 @@ namespace Visus.DeploymentToolkit.Tasks {
             }
 
             {
-                var p = System.IO.Path.Combine(this.Path, Layout.BootFilePath);
+                var p = SysPath.Combine(this.Path, Layout.BootFilePath);
                 this._logger.LogInformation("Creating directory {Path} for "
                     + "boot files.", p);
                 await this._directory.CreateAsync(p).ConfigureAwait(false);
@@ -81,7 +108,7 @@ namespace Visus.DeploymentToolkit.Tasks {
             }
 
             {
-                var p = System.IO.Path.Combine(this.Path, Layout.BootstrapperPath);
+                var p = SysPath.Combine(this.Path, Layout.BootstrapperPath);
                 this._logger.LogInformation("Creating directory {Path} for "
                     + "bootstrapper binaries.", p);
                 await this._directory.CreateAsync(p).ConfigureAwait(false);
@@ -89,7 +116,7 @@ namespace Visus.DeploymentToolkit.Tasks {
             }
 
             {
-                var p = System.IO.Path.Combine(this.Path, Layout.DriverPath);
+                var p = SysPath.Combine(this.Path, Layout.DriverPath);
                 this._logger.LogInformation("Creating directory {Path} for "
                     + "drivers.", p);
                 await this._directory.CreateAsync(p).ConfigureAwait(false);
@@ -97,7 +124,7 @@ namespace Visus.DeploymentToolkit.Tasks {
             }
 
             {
-                var p = System.IO.Path.Combine(this.Path, Layout.InstallImagePath);
+                var p = SysPath.Combine(this.Path, Layout.InstallImagePath);
                 this._logger.LogInformation("Creating directory {Path} for "
                     + "image files.", p);
                 await this._directory.CreateAsync(p).ConfigureAwait(false);
@@ -105,7 +132,7 @@ namespace Visus.DeploymentToolkit.Tasks {
             }
 
             {
-                var p = System.IO.Path.Combine(this.Path, Layout.TaskSequencePath);
+                var p = SysPath.Combine(this.Path, Layout.TaskSequencePath);
                 this._logger.LogInformation("Creating directory {Path} for "
                     + "task sequences.", p);
                 await this._directory.CreateAsync(p).ConfigureAwait(false);
@@ -113,16 +140,50 @@ namespace Visus.DeploymentToolkit.Tasks {
             }
 
             {
-                var p = System.IO.Path.Combine(this.Path, Layout.TemplatesPath);
+                var p = SysPath.Combine(this.Path, Layout.TemplatesPath);
                 this._logger.LogInformation("Creating directory {Path} for "
                     + "template files.", p);
                 await this._directory.CreateAsync(p).ConfigureAwait(false);
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+
+            if (Directory.Exists(this.Agent)) {
+                var dst = SysPath.Combine(this.Path, Layout.BinaryPath);
+                this._logger.LogInformation("Copying agent binaries from "
+                    + "{Source} to {Destination} on the deployment share.",
+                    this.Agent, dst);
+                await this._copy.CopyAsync(this.Agent,
+                    dst,
+                    CopyFlags.Recursive | CopyFlags.Overwrite);
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+
+            if (Directory.Exists(this.Bootstrapper)) {
+                var dst = SysPath.Combine(this.Path, Layout.BootstrapperPath);
+                this._logger.LogInformation("Copying bootstrapper binaries "
+                    + "from {Source} to {Destination} on the deployment share.",
+                    this.Agent, dst);
+                await this._copy.CopyAsync(this.Bootstrapper,
+                    dst,
+                    CopyFlags.Recursive | CopyFlags.Overwrite);
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+
+            if (Directory.Exists(this.Templates)) {
+                var dst = SysPath.Combine(this.Path, Layout.TemplatesPath);
+                this._logger.LogInformation("Copying template files from "
+                    + "{Source} to {Destination} on the deployment share.",
+                    this.Agent, dst);
+                await this._copy.CopyAsync(this.Templates,
+                    dst,
+                    CopyFlags.Recursive | CopyFlags.Overwrite);
                 cancellationToken.ThrowIfCancellationRequested();
             }
         }
         #endregion
 
         #region Private fields
+        private readonly ICopy _copy;
         private readonly IDirectory _directory;
         #endregion
     }
